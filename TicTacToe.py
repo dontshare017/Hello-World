16353fd02d87b420fe838b1c9fdcf3d6cd1cdd17
dontshare017 <dontshare017@users.noreply.github.com>
2020-09-08 11:48:29 -0700

    Refinement

--- TicTacToe.py
+++ TicTacToe.py
@@ -15,6 +15,7 @@ def start():
     CPU = 'O'
     stepsLeft = 5
     import random
+    import os
 
     for turn in range(0,6):
         showBoard()
@@ -26,8 +27,13 @@ def start():
 
         if board[str(move)] == ' ':
             board[str(move)] = player
-            stepsLeft -= 1
-            print("You have " + str(stepsLeft) + " steps left. Please choose your move")
+            if stepsLeft == 1:
+                stepsLeft -=1
+                break
+            else:
+                stepsLeft -= 1
+                print("You have " + str(stepsLeft) + " steps left. Please choose your move")
+
 
             CPUMove = random.randint(1,9)
             if board[str(CPUMove)] == ' ':
@@ -39,11 +45,11 @@ def start():
                 else:
                     board[str(CPUMove)] = CPU
 
-        else:
+        elif board[str(move)] != ' ':
             print('This space is already filled. Choose another destination')
             continue
 
-        if stepsLeft <=3 :
+        if stepsLeft <=2:
             if board['1'] == board['2'] == board ['3'] != ' ' or board['1'] == board['4'] == board ['7'] != ' ' or board['1'] == board['5'] == board ['9'] != ' ':
                 showBoard()
                 if board['1'] == player:
@@ -83,10 +89,48 @@ def start():
                 else:
                     print('Oh no. You lost to the CPU :(')
                 break
+            else:
+                if stepsLeft == 0:
+                    print('Game over! Looks like no one won!')
 
-            if stepsLeft == 1:
-                print('Game over! Looks like no one won!')
-                break
+    if stepsLeft == 0:
+        if board['1'] == board['2'] == board ['3'] != ' ' or board['1'] == board['4'] == board ['7'] != ' ' or board['1'] == board['5'] == board ['9'] != ' ':
+            showBoard()
+            if board['1'] == player:
+                print('Congratulation! You have won!')
+            else:
+                print('Oh no. You lost to the CPU :(')
+
+        elif board['2'] == board['5'] == board ['8'] != ' ':
+            showBoard()
+            if board['2'] == player:
+                print('Congratulation! You have won!')
+            else:
+                print('Oh no. You lost to the CPU :(')
+
+        elif board['3'] == board['6'] == board ['9'] != ' ' or board['3'] == board['5'] == board ['7'] != ' ':
+            showBoard()
+            if board['3'] == player:
+                print('Congratulation! You have won!')
+            else:
+                print('Oh no. You lost to the CPU :(')
+
+        elif board['4'] == board['5'] == board ['6'] != ' ':
+            showBoard()
+            if board['4'] == player:
+                print('Congratulation! You have won!')
+            else:
+                print('Oh no. You lost to the CPU :(')
+
+        elif board['7'] == board['8'] == board ['9'] != ' ':
+            showBoard()
+            if board['7'] == player:
+                print('Congratulation! You have won!')
+            else:
+                print('Oh no. You lost to the CPU :(')
+        else:
+            print('Game over! Looks like no one won!')
+
 
 start()
 
@@ -97,7 +141,20 @@ if str(playerChoice) == 'Y':
              '4': ' ', '5': ' ', '6': ' ',
              '7': ' ', '8': ' ', '9': ' '}
     start()
-elif str(playerChoice) != 'N' or str(playerChoice) == 'Y':
-    print('You did not enter a valid answer.')
 
-input = raw_input()
+elif str(playerChoice) == 'N':
+    print('Alrighty! Have a good day!')
+    input(' ')
+elif str(playerChoice) != 'N' and str(playerChoice) != 'Y':
+    while str(playerChoice) != 'N' and str(playerChoice) != 'Y':
+        print('You did not enter a valid answer.')
+        playerChoice = input()
+        if str(playerChoice) == 'Y':
+            board = {'1': ' ', '2': ' ', '3': ' ',
+                     '4': ' ', '5': ' ', '6': ' ',
+                     '7': ' ', '8': ' ', '9': ' '}
+            start()
+            os.system('pause')
+        elif str(playerChoice) == 'N':
+            input('Sad to see you go :(, how about you!?')
+            print('Alrighty! Have a good day!')
